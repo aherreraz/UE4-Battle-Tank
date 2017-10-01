1202376d1ceb701cc0754e55d163a64852ce857f
Andres Herrera <arhz29@gmail.com>
2017-10-01 07:40:31 -0500

    Tank Tracks - Left track throttle - Right track throttle

---
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle = %f"), *GetName(), Throttle);
+}
+
---
+++ BattleTanks/Source/BattleTanks/Public/TankTrack.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+UCLASS(ClassGroup = (TankComponent), meta = (BlueprintSpawnableComponent))
+class BATTLETANKS_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+};
